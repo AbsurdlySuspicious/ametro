81b657021fdda6ab9fcf0f6bfdd2b5e86ca5e2b0
mfgm2 <repom2@riseup.net>
2021-12-08 09:51:00 +0300

    replace repo url

--- app/src/main/java/org/ametro/app/Constants.java
+++ app/src/main/java/org/ametro/app/Constants.java
@@ -3,7 +3,7 @@ package org.ametro.app;
 import java.net.URI;
 
 public class Constants {
-    public static final URI MAP_SERVICE_URI = URI.create("https://maps.ametro.org/");
+    public static final URI MAP_SERVICE_URI = URI.create("https://absurdlysuspicious.github.io/ametro-services/repo/");
 
     public static final long MAP_EXPIRATION_PERIOD_MILLISECONDS = 1000 * 60 * 60 * 24;
 
