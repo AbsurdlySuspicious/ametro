904fa0ee55fb895c6a2cc2bc2d84ab3b54bea097
mfgm2 <repom2@riseup.net>
2022-02-12 04:03:07 +0300

    no route bottom panel layout

--- app/src/main/java/org/ametro/ui/bottom_panel/MapBottomPanelRoute.kt
+++ app/src/main/java/org/ametro/ui/bottom_panel/MapBottomPanelRoute.kt
@@ -6,7 +6,6 @@ import android.graphics.drawable.Drawable
 import android.graphics.drawable.GradientDrawable
 import android.text.format.DateFormat
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -31,7 +30,6 @@ import org.ametro.model.entities.MapSchemeLine
 import org.ametro.model.entities.MapSchemeStation
 import org.ametro.utils.StringUtils
 import org.ametro.utils.misc.AnimUtils
-import org.ametro.utils.misc.epsilonEqual
 import org.ametro.utils.misc.saturate
 import java.util.*
 import kotlin.math.max
